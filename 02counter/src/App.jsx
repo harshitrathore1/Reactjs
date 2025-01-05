3d2884f041aab8ba2514fe46f7aae87a6159966a
harshit <harshitrathore8118@gmail.com>
2025-01-05 23:35:01 +0530

    update counter and tailProps

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   
   let [counter, setCounter]=useState(15)
   //let counter=5
+
+  // // simple
+  // const addValue=()=>{
+  //   setCounter(counter+1);
+  // }
+
+  // const removeValue=()=>{
+  //   setCounter(counter-1);
+  // }
 
   const addValue = () => {
     // console.log("clicked", counter);
@@ -17,7 +24,6 @@ function App() {
     } 
     else {
       setCounter(counter+1)
-      console.log(counter);
     }
   }
 
@@ -28,7 +34,6 @@ function App() {
     } 
     else {
       setCounter(counter-1)
-    
     }
   }
 
