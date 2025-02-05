3385cc554dc5d82fb837b48ba95e1dc6b1e5aa45
harshit <harshitrathore8118@gmail.com>
2025-02-05 08:53:06 +0530

    context and local storage done

--- 09themeswitcher/src/Contexts/theme.js
+++ 09themeswitcher/src/Contexts/theme.js
@@ -1,12 +1,6 @@
 import { createContext, useContext} from 'react';
 
-export const ThemeContext = createContext(
-    {
-        themeMode:"light",
-        darkTheme:() => {},
-        lightTheme:() => {},
-    }
-)
+export const ThemeContext = createContext({themeMode:"light", darkTheme:() => {}, lightTheme:() => {}})
 
 export const ThemeProvider = ThemeContext.Provider
 
